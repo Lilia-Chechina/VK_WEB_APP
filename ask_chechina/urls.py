49cb827de7dfea96c368e234d23165e27f7904e1
Lilia-Chechina <lilya-chechina@mail.ru>
2024-11-13 23:21:31 +0300

    4_update

--- ask_chechina/urls.py
+++ ask_chechina/urls.py
@@ -14,17 +14,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+# from app import views
+#
+# urlpatterns = [
+#     path('', views.index, name='index'),  # не указываем никакого названия страницы, т.к это будет заглавная страница
+#     path('admin/', admin.site.urls),
+#     # path('hot/', views.hot, name='hot'),
+#     # path('tag/<str:tag>/', views.tag, name='tag'),
+#     path('question/<int:question_id>/', views.question, name='question'),
+#     path('login/', views.login, name='login'),
+#     path('signup/', views.signup, name='signup'),
+#     path('ask/', views.ask, name='ask'),
+# ]
+
 from django.contrib import admin
-from django.urls import path
-from app import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),  # не указываем никакого названия страницы, т.к это будет заглавная страница
     path('admin/', admin.site.urls),
-    # path('hot/', views.hot, name='hot'),
-    # path('tag/<str:tag>/', views.tag, name='tag'),
-    path('question/<int:question_id>/', views.question, name='question'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('ask/', views.ask, name='ask'),
+    path('', include('app.urls')),
 ]

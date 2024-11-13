e27a3dc1d2b94f544ae34f8ff93f3a7b5cbdd34b
Lilia-Chechina <lilya-chechina@mail.ru>
2024-11-13 20:19:57 +0300

    2_update

--- ask_chechina/asgi.py
+++ ask_chechina/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for askme_chechina project.
+ASGI config for ask_chechina project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askme_chechina.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ask_chechina.settings')
 
 application = get_asgi_application()

1e28ab58daeb217180e5bf31f69f8aa0d246b68b
Lilia-Chechina <lilya-chechina@mail.ru>
2024-11-13 22:45:19 +0300

    4_update

--- app/views.py
+++ app/views.py
@@ -2,18 +2,45 @@ from django.shortcuts import render
 
 # Это из ТЗ 2 вк имитация БД
 QUESTIONS = [
-  {
-    'title': f'title {i}',
-    'id': i,
-    'text': 'This is text for question {i}'
-  } for i in range(1, 30)
+    {
+        'title': f'title {i}',
+        'id': i,
+        'text': 'This is text for question {i}'
+    } for i in range(1, 30)
 ]
+
 
 def index(request):  # принимает на вход запрос от пользователя
     return render(
-      request, 'index.html',
-      context={'questions': QUESTIONS}
+        request, 'index.html',
+        context={'questions': QUESTIONS}
     )
-# Вы создаете словарь, где ключом является строка 'questions', а значением — список вопросов.
+
+
+# Cоздаём словарь, где ключом является строка 'questions', а значением — список вопросов.
 # В шаблоне index.html
-# вы можете получить доступ к этому списку, используя синтаксис шаблонов Django.
+# Можно получить доступ к этому списку, используя синтаксис шаблонов Django.
+
+def hot(request):
+    return render(request, 'hot.html', context={'questions': QUESTIONS})
+
+
+def tag(request, tag):
+    return render(request, 'tag.html', context={'questions': QUESTIONS, 'tag': tag})
+
+
+def question(request, question_id):
+    return render(request, 'question.html', context={'question': question})
+
+
+def login(request):
+    return render(request, 'login.html', context=None)
+
+
+def signup(request):
+    return render(request, 'signup.html', context=None)
+
+
+def ask(request):
+    return render(request, 'ask.html', context=None)
+# мб убрать None
--- ask_chechina/urls.py
+++ ask_chechina/urls.py
@@ -21,4 +21,10 @@ from app import views
 urlpatterns = [
     path('', views.index, name='index'),  # не указываем никакого названия страницы, т.к это будет заглавная страница
     path('admin/', admin.site.urls),
+    # path('hot/', views.hot, name='hot'),
+    # path('tag/<str:tag>/', views.tag, name='tag'),
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('ask/', views.ask, name='ask'),
 ]

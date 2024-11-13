6ea8681e5a69485a8727296ed8278e4332122a53
Lilia-Chechina <lilya-chechina@mail.ru>
2024-11-13 23:50:22 +0300

    5_update

--- ask_chechina/settings.py
+++ ask_chechina/settings.py
@@ -75,13 +75,16 @@ WSGI_APPLICATION = 'ask_chechina.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Users_of_Ask',
+#         'USER': 'myuser',
+#         'PASSWORD': 'vk_mai_aviasales',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

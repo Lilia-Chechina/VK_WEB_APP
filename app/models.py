f784d8ff7ec915c5ae51457e059c1cacae674dd0
Lilia-Chechina <lilya-chechina@mail.ru>
2024-11-27 18:58:40 +0300

    n update

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    # id создаётся автоматически
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=255)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, default='avatars/default.jpg')
+    # будет сохраняться в папке avatar
+
+
+def __str__(self):
+    return self.nickname
+
--- ask_chechina/settings.py
+++ ask_chechina/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'ask_chechina.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Users_of_Ask',
-#         'USER': 'myuser',
-#         'PASSWORD': 'vk_mai_aviasales',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ask_pupkin_data',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',  # порт PostgreSQL
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
